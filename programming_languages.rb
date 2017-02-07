08e95fe24b366bb77591085f41c12c297cc7be6c
Max Eingorn <meingorn@gmail.com>
2017-02-07 02:23:10 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, info|
+    info.each do |language, type|
+      if new_hash[language] == nil
+        new_array = []
+        new_array << style
+        new_hash[language] = {type:type.values.join, style:new_array}
+      else
+        new_array = new_hash[language][:style]
+        new_array << style
+        new_hash[language] = {type:type.values.join, style:new_array}
+      end
+    end
+  end
+  new_hash
 end
